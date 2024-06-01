f8c94ae08279c1d51be1c0e243bb8b33a331be5c
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-06-01 16:11:29 +0300

    second commit

---
+++ src/main/java/com/tpe/repository/HotelRepository.java
@@ -0,0 +1,27 @@
+package com.tpe.repository;
+
+import com.tpe.config.HibernateUtils;
+import com.tpe.domain.Hotel;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class HotelRepository {
+
+    private Session session;
+
+    public void save(Hotel hotel){
+        try {
+            session = HibernateUtils.getSessionFactory().openSession();
+            Transaction transaction = session.beginTransaction();
+            session.save(hotel);
+            transaction.commit();
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            HibernateUtils.closeSession(session);
+        }
+
+
+    }
+}
---
+++ src/main/java/com/tpe/service/HotelService.java
@@ -0,0 +1,11 @@
+package com.tpe.service;
+
+import com.tpe.repository.HotelRepository;
+
+import java.util.Scanner;
+
+public class HotelService {
+
+    private  Scanner scanner = new Scanner(System.in);
+    private  HotelRepository hotelRepository;
+}
