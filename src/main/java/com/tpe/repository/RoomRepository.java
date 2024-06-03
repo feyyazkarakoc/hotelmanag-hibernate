f08c8bdb8880e4934a617e503435c082ef45243d
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-06-03 16:16:22 +0300

    commit

--- src/main/java/com/tpe/repository/RoomRepository.java
+++ src/main/java/com/tpe/repository/RoomRepository.java
@@ -1,4 +1,62 @@
 package com.tpe.repository;
 
+import com.tpe.config.HibernateUtils;
+import com.tpe.domain.Room;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import java.util.List;
+
+
 public class RoomRepository {
+
+    private Session session;
+
+
+    public void save(Room room){
+        try {
+            session = HibernateUtils.getSessionFactory().openSession();
+            session.save(room);
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        }finally {
+            HibernateUtils.closeSession(session);
+        }
+    }
+
+    public Room findRoomById(Long id){
+
+        try {
+            session = HibernateUtils.getSessionFactory().openSession();
+            return session.get(Room.class,id);
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        }finally {
+            HibernateUtils.closeSession(session);
+        }
+        return null;
+    }
+
+
+
+    public List<Room>  findAll(){
+
+        try {
+            session = HibernateUtils.getSessionFactory().openSession();
+            return session.createQuery("FROM Room").getResultList();
+        } catch (HibernateException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+
+
+    }
+
+
+
+
+
+
+
 }
